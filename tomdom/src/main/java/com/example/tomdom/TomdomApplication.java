5bbf6cdaab1099f11a058ed5c3af814df943641c
apoorvkhare259 <aman.khare64@gmail.com>
2022-08-06 14:15:25 +0530

    added code files

---
+++ tomdom/src/main/java/com/example/tomdom/TomdomApplication.java
@@ -0,0 +1,13 @@
+package com.example.tomdom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomdomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomdomApplication.class, args);
+	}
+
+}
---
+++ tomdom/src/test/java/com/example/tomdom/TomdomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tomdom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomdomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
